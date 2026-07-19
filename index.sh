7b7e5a66bd90e8054f35ee261676291335c2cea7
mazhugasergei <ghbdtnghbdtn8@gmail.com>
2026-07-19 19:18:23 +1000

    fix: reboot command

--- index.sh
+++ index.sh
@@ -8,4 +8,4 @@ setup_fastfetch
 setup_sudoers "$(get_real_user)"
 install_desktop_packages
 logger done "Done."
-confirm "Reboot now?" "Y" && reboot
+confirm "Reboot now?" "Y" && "sudo reboot now" || logger info "Reboot later to apply changes."
